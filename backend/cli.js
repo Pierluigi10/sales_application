e269a39ee0040dddcb924b17ffacb1a9f32bb163
pierluigi <pierluigibaiano@gmail.com>
2021-10-25 18:17:12 +0200

    create a function to get api data and convert to the format we want

--- backend/cli.js
+++ backend/cli.js
@@ -19,37 +19,42 @@ import fetch from "node-fetch";
 //   .catch((err) => console.log("Error", err));
 
 //Alternative with await
-const API_URL =
-  "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json";
+// const API_URL =
+//   "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json";
 
-const res = await fetch(API_URL); // Fetch to API
-const data = await res.json(); // parse JSON in js object
-console.log(data);
-
-
-
+// const res = await fetch(API_URL); // Fetch to API
+// const data = await res.json(); // parse JSON in js object
+// console.log(data);
 
 const getRandomNumInRange = (minimum, maximum) => {
-    return Math.floor(Math.random() * (maximum - minimum)) + minimum;
-  };
+  return Math.floor(Math.random() * (maximum - minimum)) + minimum;
+};
 
+// get API data and convert to the format we want
+const getApiDataAndSaveFormatted = async () => {
+  const API_URL =
+    "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json";
 
+  const res = await fetch(API_URL); // Fetch to API
+  const data = await res.json(); // parse JSON in js object
+  console.log(data);
 
+  // data.forEach() // to want to loop through entries and make something for every entry
+  // data.map() to make all entries in a NEW format
+  const arrSalesPeople = data.map((person) => {
+    const personFormatNew = {
+      id: person.employeeID,
+      firstName: person.firstName,
+      lastName: person.lastName,
+      salesInEuro: getRandomNumInRange(100, 900),
+    };
+    return personFormatNew;
+  });
 
-// data.forEach() // to want to loop through entries and make something for every entry
-// data.map() to make all entries in a NEW format
+  console.log(arrSalesPeople);
+};
 
-const arrSalesPeople = data.map((person) => {
-  const personFormatNew = {
-    id: person.employeeID,
-    firstName: person.firstName,
-    lastName: person.lastName,
-    salesInEuro: getRandomNumInRange(100, 900)
-  };
-  return personFormatNew;
-});
-
-console.log(arrSalesPeople);
+getApiDataAndSaveFormatted();
 
 /* INPUT
       "employeeID": 2,
