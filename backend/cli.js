6fa6c0917a8f1271122e8b3f970fe99eaaad28e2
pierluigi <pierluigibaiano@gmail.com>
2021-10-25 17:44:35 +0200

    fetch the data from the Northwind

--- backend/cli.js
+++ backend/cli.js
@@ -7,3 +7,21 @@ import fetch from "node-fetch";
 
 // Top Sales receive  Bonus
 // bonus: 10-25 (random)
+
+// const API_URL =
+//   "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json";
+
+// fetch(API_URL)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => console.log("Error", err));
+
+//Alternative with await
+const API_URL =
+  "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json";
+
+const res = await fetch(API_URL); // Fetch to API
+const data = await res.json(); // parse JSON in js object
+console.log(data);
