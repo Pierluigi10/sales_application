062985c7cabc7a0ed8fbdf162223b39cc545a5b3
pierluigi <pierluigibaiano@gmail.com>
2021-10-25 17:21:29 +0200

    plan the work to be done

---
+++ backend/dataUpdater.js
@@ -0,0 +1,7 @@
+// CLI: API Data fetcher and  updater
+
+// fetch the data from the Northwind
+// from this data, create the following two files: salespersons.json and topSalesperson.json
+
+// Top Sales receive  Bonus
+// bonus: 10-25 (random)
---
+++ backend/server.js
@@ -0,0 +1,6 @@
+//API
+
+// 2 routes
+
+// Route 1: / salesperson => read file sales.json
+// Route 2: / topsalesperson => read file topsales.json
